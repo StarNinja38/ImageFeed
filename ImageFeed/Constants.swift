c636a35e1d7c7df998c4e8fbcd43d982a1b4ab9a
starninja <mike.zhura@gmail.com>
2026-08-24 12:27:58 +0300

    Ключи приложения Unsplash

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "PASTE_YOUR_ACCESS_KEY"
-    static let secretKey = "PASTE_YOUR_SECRET_KEY"
+    static let accessKey = "DdryzG6oaDYWKRY_5Ur-hyJhPgLXZvkG49RSct53g8k"
+    static let secretKey = "HzXgQXW3f_K2bSnQZBRParU-EDpKgIOTvefXXnKxto0"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes+read_photos"
     static let defaultBaseURLString = "https://api.unsplash.com"
